fbaa11835538c301c5c5e0db6cc560498d0e8aab
Daniel <danielshoemark@gmail.com>
2020-02-14 00:47:10 +0000

    ALL PILLAR INTERACTIONS DONE

--- Assets/Scripts/World_Controller.cs
+++ Assets/Scripts/World_Controller.cs
@@ -21,10 +21,15 @@ public class World_Controller : MonoBehaviour
     public float shortestDistance = 999f;
     private GameObject bestAnt;
     public LineRenderer bestAntLine;
+    public bool pressButton = false;
+    public Canvas buttons;
+    public bool spawnPillars = false;
+    public bool destroyPillars = false;
 
     // Use this for initialization
     void Start()
     {
+        buttons.enabled = false;
         bTSP = new Basic_TSP(floor.GetComponent<MeshRenderer>().bounds.size);
         SpawnPillarsAndAnts();
     }
@@ -33,12 +38,14 @@ public class World_Controller : MonoBehaviour
     void Update()
     {
         CheckAntsAreDone();
+        SpawnPillar();
+        DestroyPillar();
     }
 
     void CheckAntsAreDone()
     {
         int antsDone = 0;
-        for (int i = 0; i < antAmount; i++)
+        for (int i = 0; i < antInstances.Count; i++)
         {
             if (antInstances[i].GetComponent<AntMovement>().noMoreMoves == true)
             {
@@ -81,13 +88,8 @@ public class World_Controller : MonoBehaviour
                 Destroy(antInstances[0].gameObject);
                 antInstances.Remove(antInstances[0]);
             }
-            int rLocation;
-            for (int i = 0; i < antAmount; i++)
-            {
-                rLocation = Random.Range(0, pillars.Count);
-                antInstances.Add(Instantiate(antPrefab, pillars[rLocation].transform.position, Quaternion.identity));
-                antInstances[i].GetComponent<AntMovement>().newDestination = pillars[rLocation];
-            }
+            buttons.enabled = true;
+            pressButton = true;
             Debug.Log(shortestDistance);
         }
     }
@@ -101,7 +103,7 @@ public class World_Controller : MonoBehaviour
             GameObject p = Instantiate(pillarPrefab, bTSP.pPositions[i], Quaternion.identity);
             pillars.Add(p);
         }
-        for (int i = 0; i <pillars.Count; i++)
+        for (int i = 0; i < pillars.Count; i++)
         {
             for (int j = 0; j < pillars.Count; j++)
             {
@@ -117,5 +119,93 @@ public class World_Controller : MonoBehaviour
             antInstances.Add(Instantiate(antPrefab, pillars[rLocation].transform.position, Quaternion.identity));
             antInstances[i].GetComponent<AntMovement>().newDestination = pillars[rLocation];
         }
+    }
+
+    void SpawnPillar()
+    {
+        if (spawnPillars == true)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Vector3 mousePosition = Input.mousePosition;
+                mousePosition.z = 89;
+                mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+                Vector3 tempFloor = floor.GetComponent<MeshRenderer>().bounds.size;
+                if (Mathf.Abs(mousePosition.x) < tempFloor.x / 2 && Mathf.Abs(mousePosition.z) < tempFloor.z / 2)
+                {
+                    GameObject p = Instantiate(pillarPrefab, new Vector3(mousePosition.x, 0.0f, mousePosition.z), Quaternion.identity);
+                    for (int i = 0; i < pillars.Count; i++)
+                    {
+                        pillars[i].GetComponent<Edge_Controller>().edges.Add(new Edge(p, p.transform.position));
+                        p.GetComponent<Edge_Controller>().edges.Add(new Edge(pillars[i], pillars[i].transform.position));
+                    }
+                    pillars.Add(p);
+                    shortestDistance = 999f;
+                }
+            }
+        }
+    }
+
+    void DestroyPillar()
+    {
+        if (destroyPillars == true)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Vector3 mousePosition = Input.mousePosition;
+                mousePosition.z = 89;
+                mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+                for (int i = 0; i < pillars.Count; i++)
+                {
+                    if (Vector3.Distance(pillars[i].transform.position, mousePosition) < 1.0f)
+                    {
+                        for (int j = 0; j < pillars.Count; j++)
+                        {
+                            for (int k = 0; k < pillars[j].GetComponent<Edge_Controller>().edges.Count; k++)
+                            {
+                                if (pillars[j].GetComponent<Edge_Controller>().edges[k].pillarLocation == pillars[i].transform.position)
+                                {
+                                    pillars[j].GetComponent<Edge_Controller>().edges.Remove(pillars[j].GetComponent<Edge_Controller>().edges[k]);
+                                    k = pillars[j].GetComponent<Edge_Controller>().edges.Count;
+                                }
+                            }
+                        }
+                        Destroy(pillars[i].gameObject);
+                        pillars.Remove(pillars[i]);
+                        i = pillars.Count + 1;
+                    }
+                }
+            }
+        }
+    }
+
+    public void SpawnAnts()
+    {
+        if (pressButton == true)
+        {
+            int rLocation;
+            for (int i = 0; i < antAmount; i++)
+            {
+                rLocation = Random.Range(0, pillars.Count);
+                antInstances.Add(Instantiate(antPrefab, pillars[rLocation].transform.position, Quaternion.identity));
+                antInstances[i].GetComponent<AntMovement>().newDestination = pillars[rLocation];
+            }
+            pressButton = false;
+            spawnPillars = false;
+            destroyPillars = false;
+            buttons.enabled = false;
+        }
+    }
+
+    public void SpawnPillarButton()
+    {
+        spawnPillars = true;
+        destroyPillars = false;
+    }
+
+    public void DestroyPillarButton()
+    {
+        destroyPillars = true;
+        spawnPillars = false;
     }
 }
