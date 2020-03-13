dd2961b3c5dca1ceb764951e9df3406d49381772
Daniel <danielshoemark@gmail.com>
2020-03-13 04:34:38 +0000

    Adding a comparison technique

---
+++ Assets/Scripts/Neighbour.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Neighbour
+{
+    public GameObject startPosition;
+    public GameObject newPosition;
+    public int runTime;
+
+    private int smallestDistancePointer;
+    private float smallestDistance;
+
+    public List<GameObject> beenToPlaces = new List<GameObject>();
+    public float path = 0.0f;
+    public List<GameObject> waypoints = new List<GameObject>();
+    bool skip = false;
+    //public List<GameObject> waypointCopy = new List<GameObject>();
+    //public List<float> edgeProbabilities/* = new List<float>()*/;
+    //float totalEdgeProbabilities = 0.0f;
+    //GameObject currentPillar;
+
+    public void Move()
+    {
+        //waypointCopy = waypoints;
+        //Debug.Log("Run time = " + runTime);
+        //Debug.Log("Amount of pillars = " + waypoints.Count);
+        for (int j = 0; j < runTime; j++)
+        {
+            smallestDistance = 99999f;
+            for (int i = 0; i < waypoints.Count; i++)
+            {
+                if (waypoints[i].transform.position == startPosition.transform.position)
+                {
+                    beenToPlaces.Add(waypoints[i]);
+                    //waypointCopy.Remove(waypointCopy[i]);
+                    i = waypoints.Count;
+                }
+            }
+            for (int i = 0; i < waypoints.Count; i++)
+            {
+                for (int k = 0; k < beenToPlaces.Count; k++)
+                {
+                    if (waypoints[i].transform.position == beenToPlaces[k].transform.position)
+                    {
+                        skip = true;
+                        k = waypoints.Count;
+                    }
+                    else
+                    {
+                        skip = false;
+                    }
+                }
+                if (Vector3.Distance(startPosition.transform.position, waypoints[i].transform.position) < smallestDistance && skip == false)
+                {
+                    //Debug.Log(Vector3.Distance(startPosition.transform.position, waypointCopy[i].transform.position));
+                    smallestDistance = Vector3.Distance(startPosition.transform.position, waypoints[i].transform.position);
+                    smallestDistancePointer = i;
+                }
+            }
+            if (smallestDistance == 99999f)
+            {
+                smallestDistance = 0.0f;
+            }
+            //Debug.Log("Smallest distance in the loop = " + smallestDistance);
+            path += smallestDistance;
+            //Debug.Log("After adding my value of path is " + path);
+            startPosition = waypoints[smallestDistancePointer];
+        }
+        //Debug.Log("Path to traverse everywhere = " + path);
+
+        //if (noMoreMoves == false)
+        //{
+        //    if (transform.position == newDestination.transform.position)
+        //    {
+        //        beenToPlaces.Add(newDestination);
+        //        for (int i = 0; i < waypointCopy.Count; i++)
+        //        {
+        //            if (waypointCopy[i].transform.position == transform.position)
+        //            {
+        //                currentPillar = waypointCopy[i];
+        //                waypointCopy.Remove(waypointCopy[i]);
+        //            }
+        //        }
+        //        if (waypointCopy.Count == 0)
+        //        {
+        //            noMoreMoves = true;
+        //            return;
+        //        }
+        //        for (int i = 0; i < currentPillar.GetComponent<Edge_Controller>().edges.Count; i++)
+        //        {
+        //            for (int j = 0; j < waypointCopy.Count; j++)
+        //            {
+        //                if (currentPillar.GetComponent<Edge_Controller>().edges[i].pillarLocation == waypointCopy[j].transform.position)
+        //                {
+        //                    float scent = currentPillar.GetComponent<Edge_Controller>().edges[i].edgeScent;
+        //                    float visibility = 1 / Vector3.Distance(currentPillar.GetComponent<Edge_Controller>().edges[i].pillarLocation, transform.position);
+        //                    edgeProbabilities.Add(scent * Mathf.Pow(visibility, 5));
+        //                    totalEdgeProbabilities += scent * Mathf.Pow(visibility, 5);
+        //                }
+        //            }
+        //        }
+        //        int currentMax = 0;
+        //        float maxValue = 0.0f;
+        //        for (int i = 0; i < edgeProbabilities.Count; i++)
+        //        {
+        //            edgeProbabilities[i] = edgeProbabilities[i] / (totalEdgeProbabilities - edgeProbabilities[i]);
+        //            if (edgeProbabilities[i] > maxValue)
+        //            {
+        //                currentMax = i;
+        //                maxValue = edgeProbabilities[i];
+        //            }
+        //        }
+        //        newDestination = waypointCopy[currentMax];
+        //        tourLength += Vector3.Distance(newDestination.transform.position, transform.position);
+        //    }
+        //    transform.position = Vector3.MoveTowards(transform.position, newDestination.transform.position, speed * Time.deltaTime);
+        //    edgeProbabilities.Clear();
+        //}
+    }
+}
--- Assets/Scripts/World_Controller.cs
+++ Assets/Scripts/World_Controller.cs
@@ -16,28 +16,37 @@ public class World_Controller : MonoBehaviour
 
     Basic_TSP bTSP;
 
-    private List<GameObject> pillars = new List<GameObject>();
+    public List<GameObject> pillars = new List<GameObject>();
 
-    public float shortestDistance = 999f;
+    public float shortestDistance = 99999f;
     private GameObject bestAnt;
     public LineRenderer bestAntLine;
-    public bool pressButton = false;
+    public bool pressButton = true;
     public Canvas buttons;
     public bool spawnPillars = false;
     public bool destroyPillars = false;
+    public bool runNN = true;
+    public int antAttempts = 0;
+    public float deltaTime = 0.0f;
+
+    public float nnPath;
 
     // Use this for initialization
     void Start()
     {
-        buttons.enabled = false;
+        buttons.enabled = true;
         bTSP = new Basic_TSP(floor.GetComponent<MeshRenderer>().bounds.size);
         SpawnPillarsAndAnts();
+
+        //Debug.Log("Wrong path = " + Vector3.Distance(new Vector3(-40.0f, 0.0f, 30.0f), new Vector3(0.0f, 0.0f, 0.0f)));
+        //Debug.Log("Correct path = " + Vector3.Distance(new Vector3(-40.0f, 0.0f, 30.0f), new Vector3(10.0f, 0.0f, 40.0f)));
     }
 
     // Update is called once per frame
     void Update()
     {
         CheckAntsAreDone();
+        CalculateFPS();
         SpawnPillar();
         DestroyPillar();
     }
@@ -88,15 +97,24 @@ public class World_Controller : MonoBehaviour
                 Destroy(antInstances[0].gameObject);
                 antInstances.Remove(antInstances[0]);
             }
-            buttons.enabled = true;
+            antAttempts++;
+            GameObject.Find("Number of Attempts").gameObject.GetComponent<UnityEngine.UI.Text>().text = "Number of Attempts = " + antAttempts;
+            GameObject.Find("Spawn Ants").GetComponentInChildren<UnityEngine.UI.Text>().text = "Spawn Ants";
+            //buttons.GetComponentWithTag<UnityEngine.UI.Text>().text = "Number of Attempts = " + antAttempts;
+            //buttons.enabled = true;
             pressButton = true;
-            Debug.Log(shortestDistance);
+            GameObject.Find("ACO Best").GetComponentInChildren<UnityEngine.UI.Text>().text = "ACO's Best Path = " + shortestDistance;
+            //if (antLoops > 0)
+            //{
+            //    SpawnAnts();
+            //    antLoops--;
+            //}
         }
     }
 
     void SpawnPillarsAndAnts()
     {
-        int rLocation;
+        //int rLocation;
         antPrefab.GetComponent<AntMovement>().waypoints = pillars;
         for (int i = 0; i < bTSP.pPositions.Length; i++)
         {
@@ -113,12 +131,12 @@ public class World_Controller : MonoBehaviour
                 }
             }
         }
-        for (int i = 0; i < antAmount; i++)
-        {
-            rLocation = Random.Range(0, pillars.Count);
-            antInstances.Add(Instantiate(antPrefab, pillars[rLocation].transform.position, Quaternion.identity));
-            antInstances[i].GetComponent<AntMovement>().newDestination = pillars[rLocation];
-        }
+        //for (int i = 0; i < antAmount; i++)
+        //{
+        //    rLocation = Random.Range(0, pillars.Count);
+        //    antInstances.Add(Instantiate(antPrefab, pillars[rLocation].transform.position, Quaternion.identity));
+        //    antInstances[i].GetComponent<AntMovement>().newDestination = pillars[rLocation];
+        //}
     }
 
     void SpawnPillar()
@@ -142,6 +160,8 @@ public class World_Controller : MonoBehaviour
                     pillars.Add(p);
                     shortestDistance = 999f;
                 }
+                antAttempts = 0;
+                GameObject.Find("Number of Attempts").gameObject.GetComponent<UnityEngine.UI.Text>().text = "Number of Attempts = " + antAttempts;
             }
         }
     }
@@ -175,14 +195,34 @@ public class World_Controller : MonoBehaviour
                         i = pillars.Count + 1;
                     }
                 }
+                antAttempts = 0;
+                GameObject.Find("Number of Attempts").gameObject.GetComponent<UnityEngine.UI.Text>().text = "Number of Attempts = " + antAttempts;
             }
         }
     }
 
     public void SpawnAnts()
     {
+        //Debug.Log("YEET");
         if (pressButton == true)
         {
+            //GameObject antButton = GameObject.Find("Spawn Ants");
+            //antButton.GetComponentInChildren<UnityEngine.UI.Text>().fontSize = 8;
+            //antButton.gameObject.transform.localScale += new Vector3(2.0f, 1.0f, 0.0f);
+            //antButton.GetComponentInChildren<UnityEngine.UI.Text>().alignment = TextAnchor.MiddleLeft;
+            //antButton.GetComponentInChildren<UnityEngine.UI.Text>().text = "Enter amount of runs: \nPlease don't enter things that aren't numbers";
+            //string amountToRun = string.Empty;
+            //Debug.Log("YEET MA MEAT");
+            //foreach (char c in Input.inputString)
+            //{
+            //amountToRun += Input.inputString;
+            //}
+            //Debug.Log("YEET MA MEAT");
+            //antLoops = int.Parse(amountToRun);
+            //antButton.gameObject.transform.localScale += new Vector3(-2.0f, -1.0f, 0.0f);
+            //antButton.GetComponentInChildren<UnityEngine.UI.Text>().alignment = TextAnchor.MiddleCenter;
+            //antButton.GetComponentInChildren<UnityEngine.UI.Text>().fontSize = 14;
+            GameObject.Find("Spawn Ants").GetComponentInChildren<UnityEngine.UI.Text>().text = "Cancel Ants";
             int rLocation;
             for (int i = 0; i < antAmount; i++)
             {
@@ -193,19 +233,81 @@ public class World_Controller : MonoBehaviour
             pressButton = false;
             spawnPillars = false;
             destroyPillars = false;
-            buttons.enabled = false;
+            runNN = false;
+            //buttons.enabled = false;
+            //antButton.GetComponentInChildren<UnityEngine.UI.Text>().text = "Cancel Ants";
+        }
+        else //If cancel ants is needed
+        {
+            for (int i = 0; i < antAmount; i++)
+            {
+                Destroy(antInstances[0].gameObject);
+                antInstances.Remove(antInstances[0]);
+            }
+            pressButton = true;
+            runNN = true;
+            GameObject.Find("Spawn Ants").GetComponentInChildren<UnityEngine.UI.Text>().text = "Spawn Ants";
         }
     }
 
     public void SpawnPillarButton()
     {
-        spawnPillars = true;
-        destroyPillars = false;
+        if (pressButton == true)
+        {
+            spawnPillars = true;
+            destroyPillars = false;
+        }
     }
 
     public void DestroyPillarButton()
     {
-        destroyPillars = true;
-        spawnPillars = false;
+        if (pressButton == true)
+        {
+            destroyPillars = true;
+            spawnPillars = false;
+        }
+    }
+
+    public void CalculateNN()
+    {
+        if (runNN == true)
+        {
+            float before = Time.realtimeSinceStartup;
+            Neighbour[] traversers = new Neighbour[pillars.Count];
+            //float[] possibleDistances = new float[pillars.Count];
+            float bestDistance = 99999f;
+            for (int i = 0; i < pillars.Count; i++)
+            {
+                traversers[i] = new Neighbour
+                {
+                    waypoints = pillars,
+                    startPosition = pillars[i],
+                    runTime = pillars.Count
+                };
+                //traversers[i].waypoints = pillars;
+                //traversers[i].startPosition = pillars[i];
+                //traversers[i].runTime = pillars.Count;
+                traversers[i].Move();
+                if (traversers[i].path < bestDistance)
+                {
+                    bestDistance = traversers[i].path;
+                }
+            }
+            GameObject.Find("NN Best").GetComponentInChildren<UnityEngine.UI.Text>().text = "NN's Best Path = " + bestDistance;
+            //Debug.Log(bestDistance);
+            runNN = false;
+            float after = Time.realtimeSinceStartup;
+            float duration = after - before;
+            GameObject.Find("NN").GetComponentInChildren<UnityEngine.UI.Text>().text = "Duration in milliseconds: " + duration;
+            //Debug.Log("Duration in milliseconds: " + duration);
+        }
+    }
+
+    public void CalculateFPS()
+    {
+        deltaTime += (Time.deltaTime - deltaTime) * 0.1f;
+        //Debug.Log(deltaTime);
+        float fps = 1.0f / deltaTime;
+        GameObject.Find("FPS").GetComponentInChildren<UnityEngine.UI.Text>().text = "FPS (VSync Enabled) = " + Mathf.Ceil(fps).ToString();
     }
 }
